65f96ced9149b07a90daba964a798f48199b1f36
Tommy <tommy.ollerenshaw@intouchnetworks.com>
2018-04-26 11:50:20 +0100

    add markup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
-let app = document.querySelector('#app')
+let app = document.querySelector('body')
 
-app.innerHTML = '<h2>Welcome to feedback-popup</h2>'
+app.innerHTML = Popup;
+
+let popup = {
+    isOpen: true,
+}
+
+function Popup() {
+    return `<div class="feedback__container">
+
+        <div class="feedback__container--inner">
+
+          <div class="feedback__header">
+
+            <h1>Support our continuous improvement.</h1>
+
+          </div>
+
+          <div class="feedback__textarea">
+          
+          <textarea autoFocus placeholder="Describe your issue or share your ideas" name="feedback" id="main-textarea"></textarea>
+
+          </div>
+
+          <div class="feedback__add-screenshot">
+
+            //   <label 
+            //   // onLoad={screenShot()} 
+            //   class="control control-checkbox">
+            //     Send screenshot
+            //     <input type="checkbox"/>
+            //     <div class="control_indicator">
+            //     </div>
+            //   </label>
+
+
+          </div>
+
+          <div class="feedback__screenshot">
+
+          </div>
+
+          <div class="feedback__confirm">
+            <ul>
+              <li>
+                <button class="btn btn-cancel">cancel</button>
+              </li>
+              <li>
+                <button class="btn btn-confirm">send</button>
+              </li>
+            </ul>
+
+          </div>
+
+
+        </div>
+
+      </div>`;
+}
