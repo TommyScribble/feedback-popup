6287ad4f2bc56b8766030d0ca73e4a4eb1710d1e
Tommy <tommy.ollerenshaw@intouchnetworks.com>
2018-04-26 14:58:20 +0100

    break markup into funcrtions

--- src/index.js
+++ src/index.js
@@ -3,58 +3,71 @@ let app = document.querySelector('body')
 app.innerHTML = Popup();
 
 let popup = {
-    isOpen: true,
+  isOpen: true,
 };
 
+function Header() {
+
+  const heading = "Support our continuous improvement.";
+
+  return `<div class="feedback__header">
+            <h1>
+            ${heading}
+            </h1>
+          </div>`;
+}
+
+function TextArea() {
+
+  const textAreaPlaceholder = "Describe your issue or share your ideas"
+
+  return `<div class="feedback__textarea">
+            <textarea autoFocus placeholder=${textAreaPlaceholder} name="feedback" id="main-textarea"></textarea>
+          </div>`;
+}
+
+function screenShot() {
+  return `<div class="feedback__add-screenshot">
+            <label 
+            {// onLoad={screenShot()} }
+            class="control control-checkbox">
+              Send screenshot
+              <input type="checkbox"/>
+              <div class="control_indicator">
+              </div>
+            </label>
+            </div>
+            <div class="feedback__screenshot">
+          </div>`;
+}
+
+function confirmation() {
+  return `<div class="feedback__confirm">
+          <ul>
+            <li>
+              <button class="btn btn-cancel">cancel</button>
+            </li>
+            <li>
+              <button class="btn btn-confirm">send</button>
+            </li>
+          </ul>
+        </div>`;
+}
+
 function Popup() {
-    return `<div class="feedback__container">
+  return `<div class="feedback__container">
 
-        <div class="feedback__container--inner">
+            <div class="feedback__container--inner">
 
-          <div class="feedback__header">
+              ${Header()}
 
-            <h1>Support our continuous improvement.</h1>
+              ${TextArea()}
 
-          </div>
+              ${screenShot()}
 
-          <div class="feedback__textarea">
-          
-          <textarea autoFocus placeholder="Describe your issue or share your ideas" name="feedback" id="main-textarea"></textarea>
+              ${confirmation()}
 
-          </div>
+            </div>
 
-          <div class="feedback__add-screenshot">
-
-              <label 
-              {// onLoad={screenShot()} }
-              class="control control-checkbox">
-                Send screenshot
-                <input type="checkbox"/>
-                <div class="control_indicator">
-                </div>
-              </label>
-
-
-          </div>
-
-          <div class="feedback__screenshot">
-
-          </div>
-
-          <div class="feedback__confirm">
-            <ul>
-              <li>
-                <button class="btn btn-cancel">cancel</button>
-              </li>
-              <li>
-                <button class="btn btn-confirm">send</button>
-              </li>
-            </ul>
-
-          </div>
-
-
-        </div>
-
-      </div>`;
+          </div>`;
 }
