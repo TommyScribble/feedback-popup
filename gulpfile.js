909053b301c9a6349661a8d5dab2d1ffa6340ed9
Tommy <tommy.ollerenshaw@intouchnetworks.com>
2018-04-30 13:29:57 +0100

    format gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 const
-  gulp = require('gulp'),
-  sass = require('gulp-sass'),
+  gulp = require("gulp"),
+  sass = require("gulp-sass"),
   watch = require("gulp-watch");
 
-gulp.task('sass-compile', function() {
-  return gulp.src('src/styles/main.scss')
+gulp.task("sass-compile", function () {
+  return gulp.src("src/styles/main.scss")
     .pipe(sass())
-    .pipe(gulp.dest('src/styles/'))
+    .pipe(gulp.dest("src/styles/"))
 });
 
-gulp.task('watch-sass', function() {
-    gulp.watch('src/styles/main.scss', [ 'sass-compile' ]);
+gulp.task("watch-sass", function () {
+  gulp.watch("src/styles/main.scss", ["sass-compile"]);
 });
 
-gulp.task('default', [ 'sass-compile' ]);
+gulp.task("default", ["sass-compile"]);
 
-gulp.task('watch', [ 'watch-sass' ]);
+gulp.task("watch", ["watch-sass"]);
