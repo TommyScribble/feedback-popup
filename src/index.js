18e0b0860930511f59e3eea103653661765862c5
Tommy <tommy.ollerenshaw@intouchnetworks.com>
2019-05-14 12:07:02 +0100

    rename index for testing

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ let feedbackPopup = new FeedbackPopup(
   "Support our continuous improvement.",
   "main-body",
   "Describe your issue or share your ideas",
-  "8fbffa50-a991-4321-80a7-680c11f36aac",
-  "tommy.ollerenshaw+feedback@intouchnetworks.com",
-  "tommy@scribbledesign.co.uk"
+  "http://localhost:3004/communications/feedbackPopup"
 );
 
 feedbackPopup.buttonWidget();
