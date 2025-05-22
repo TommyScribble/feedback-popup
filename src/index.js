9fb33181249977d824f559413e3bef74f5210a0b
Tommy <tommy@scribbledesign.co.1uk>
2025-05-22 14:22:49 +0100

    start getting the styles to load

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import FeedbackPopup from './js';
 
 let feedbackPopup = new FeedbackPopup(
 	"Spotted a glitch?",
-	"Support our continuous what this or this.",
+	"Support our continuous .",
 	"main-body",
 	"Describe your issue or share your ideas",
 	"http://localhost:3004/communications/feedbackPopup"
