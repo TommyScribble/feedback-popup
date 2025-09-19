8a418f58817e0e72e8875cecac036cb1b077196c
Tommy <tommy@scribbledesign.co.1uk>
2025-09-19 17:01:30 +0100

    build: add npm publish scripts

---
+++ src/library.ts
@@ -0,0 +1,8 @@
+import FeedbackPopup from './ts/index';
+
+// Export the main class and types
+export { default as FeedbackPopup } from './ts/index';
+export type { FeedbackPopupConfig } from './ts/index.d';
+
+// Also make it available as default export for convenience
+export default FeedbackPopup;
